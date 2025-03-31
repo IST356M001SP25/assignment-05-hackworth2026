49301c2e4a9838d624a6b9d8b566b73ffb3c5857
hackworth2026 <Ahhackwo@syr.edu>
2025-03-30 21:12:09 -0400

    commit

--- code/1_extract.py
+++ code/1_extract.py
@@ -3,5 +3,36 @@ import numpy as np
 import streamlit as st
 import pandaslib as pl
   
-#TODO Write your extraction code here
+def extract_year_mdy(date_str):
+    return pd.to_datetime(date_str).year
+
+def extract_states():
+    states_url = 'https://docs.google.com/spreadsheets/d/14wvnQygIX1eCVo7H5B7a96W1v5VCg6Q9yeRoESF6epw/export?format=csv'
+    states_data = pd.read_csv(states_url)
+    states_data.to_csv('cache/states.csv', index=False)
+    return states_data
+
+def extract_survey():
+    survey_url = 'https://docs.google.com/spreadsheets/d/1IPS5dBSGtwYVbjsfbaMCYIWnOuRmJcbequohNxCyGVw/export?resourcekey=&gid=1625408792&format=csv'
+    survey_data = pd.read_csv(survey_url)
+    survey_data['year'] = survey_data['Timestamp'].apply(extract_year_mdy)
+    survey_data.to_csv('cache/survey.csv', index=False)
+    return survey_data
+
+def extract_col_data(year):
+    col_url = f'https://www.example.com/col_{year}.csv'  # Replace with the actual URL
+    col_data = pd.read_csv(col_url)
+    col_data['year'] = year
+    col_data.to_csv(f'cache/col_{year}.csv', index=False)
+    return col_data
+
+def extract_all_data():
+    extract_states()
+    survey_data = extract_survey()
+    years = survey_data['year'].unique()
+    for year in years:
+        extract_col_data(year)
+
+if __name__ == "__main__":
+    extract_all_data()
 
--- code/2_transform.py
+++ code/2_transform.py
@@ -2,5 +2,43 @@ import pandas as pd
 import streamlit as st
 import pandaslib as pl
 
-# TODO: Write your transformation code here
+def load_data():
+    states_data = pd.read_csv('cache/states.csv')
+    survey_data = pd.read_csv('cache/survey.csv')
+    col_data_frames = []
+    for year in survey_data['year'].unique():
+        col_data_frames.append(pd.read_csv(f'cache/col_{year}.csv'))
+    col_data = pd.concat(col_data_frames, ignore_index=True)
+    return states_data, survey_data, col_data
+
+def merge_data(states_data, survey_data, col_data):
+    survey_data['_country'] = survey_data['Which country do you work in?'].apply(pl.clean_country_usa)
+    survey_states_combined = pd.merge(survey_data, states_data, left_on='If you\'re in the U.S., what state do you work in?', 
+                                      right_on='State', how='inner')
+    survey_states_combined['_full_city'] = survey_states_combined['City'] + ', ' + survey_states_combined['Abbreviation'] + ', ' + survey_states_combined['_country']
+    combined = pd.merge(survey_states_combined, col_data, how='inner', left_on=['year', '_full_city'], right_on=['year', 'City'])
+    return combined
+
+def normalize_salary(combined):
+    combined['__annual_salary_cleaned'] = combined['Annual salary'].apply(pl.clean_currency)
+    combined['_annual_salary_adjusted'] = (combined['__annual_salary_cleaned'] / combined['Cost of Living']) * 100
+    return combined
+
+def generate_reports(combined):
+    report1 = pd.pivot_table(combined, values='_annual_salary_adjusted', 
+                             index='_full_city', columns='How old are you?', aggfunc='mean')
+    report1.to_csv('cache/annual_salary_adjusted_by_location_and_age.csv')
+    
+    report2 = pd.pivot_table(combined, values='_annual_salary_adjusted', 
+                             index='_full_city', columns='What is your highest level of education?', aggfunc='mean')
+    report2.to_csv('cache/annual_salary_adjusted_by_location_and_education.csv')
+
+def transform_data():
+    states_data, survey_data, col_data = load_data()
+    combined = merge_data(states_data, survey_data, col_data)
+    normalized_data = normalize_salary(combined)
+    generate_reports(normalized_data)
+
+if __name__ == "__main__":
+    transform_data()
 
